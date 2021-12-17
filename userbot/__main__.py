f53d81d3c6630effb3c1eb8a31f2a1cea801afb9
ashit10-git <84613198+ashit10-git@users.noreply.github.com>
2021-12-18 00:14:03 +0530

    Create __main__.py

---
+++ userbot/__main__.py
@@ -0,0 +1,30 @@
+import glob
+from pathlib import Path
+from . import bot
+import sys
+import logging
+import importlib
+from pathlib import Path
+
+path = "userbot/modules/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        pathq = Path(a.name)
+        plugin_name = pathq.stem
+        load_plugins(plugin_name.replace(".py", ""))
+
+def load_plugins(plugin_name):
+    path = Path(f"userbot/modules/{plugin_name}.py")
+    name = "userbot.modules.{}".format(plugin_name)
+    spec = importlib.util.spec_from_file_location(name, path)
+    load = importlib.util.module_from_spec(spec)
+    load.logger = logging.getLogger(plugin_name)
+    spec.loader.exec_module(load)
+    sys.modules["userbot.modules." + plugin_name] = load
+ 
+
+print("Started..!")
+
+if __name__ == "__main__":
+    bot.run_until_disconnected()
