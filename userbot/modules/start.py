1da1566999901f50da22b5abd76fe5b9b664b8fe
ashit10-git <84613198+ashit10-git@users.noreply.github.com>
2021-12-18 00:14:59 +0530

    Update start.py

--- userbot/modules/start.py
+++ userbot/modules/start.py
@@ -1,4 +1,10 @@
-
+from .. import bot
+from telethon import *
+import time
+import subprocess
+import os
+import datetime
+from datetime import datetime
 
 
 @bot.on(events.NewMessage(pattern='/start'))
