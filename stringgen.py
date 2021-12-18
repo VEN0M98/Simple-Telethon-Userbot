a9c48942b60d741b0b10d5f85ac773e02c4063ad
jaaat4u <devangsingh369@gmail.com>
2021-12-18 10:02:47 +0530

    add string generator
    Signed-off-by: jaaat4u <devangsingh369@gmail.com>

---
+++ stringgen.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# (c) https://t.me/TelethonChat/37677
+# This Source Code Form is subject to the terms of the GNU
+# General Public License, v.3.0. If a copy of the GPL was not distributed with this
+# file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.
+
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+
+print(" Go to my.telegram.org & gather the APP_ID and API_HASH variables, enter them here.\n");
+
+APP_ID = int(input("Enter APP ID here: "))
+API_HASH = input("Enter API HASH here: ")
+
+with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
+    print(client.session.save())
+    client.send_message("me", client.session.save())
