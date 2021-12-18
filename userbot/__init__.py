7ec61ddd2a4034a28f57f21f10cb84c5641c009f
ashit10-git <84613198+ashit10-git@users.noreply.github.com>
2021-12-18 11:42:41 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -6,5 +6,6 @@ import time
 API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
+SESSION = config("SESSION", default=None)
 
 bot = TelegramClient('ub', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
