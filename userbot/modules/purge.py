0a09927fd0e70112eda82045432891bc9841bc49
Arthur Shelby <89604469+jaaat4u@users.noreply.github.com>
2021-12-24 15:11:21 +0530

    purge.py: fix typo

--- userbot/modules/purge.py
+++ userbot/modules/purge.py
@@ -6,7 +6,7 @@ import os
 @client.on(events.NewMessage(outgoing=True, pattern=f'{HANDLER}purge'))
 async def handler(event):
   try:
-    pu=await event.edit('`purging ...`')
+    pu=await event.edit('  Purging ...')
     chat = await event.get_input_chat() 
     id= await event.get_reply_message()
     idd = id.id 
